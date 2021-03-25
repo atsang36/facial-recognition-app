474e92fdb17e36a12b3e357e33eed38dca5c65a6
Andy Tsang <atsang36@hotmail.com>
2021-03-24 18:40:27 -0700

    initial creation of components

---
+++ src/components/logo/logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tilt from "react-tilt";
+import "./logo.css";
+import bird_logo from "./bird.png";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3 ">
+          <img alt="logo" src={bird_logo} style={{ paddingTop: "5px" }}></img>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
